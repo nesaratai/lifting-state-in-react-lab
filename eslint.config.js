14a8e0102595aa677e66ffcd9a5c74592766ac82
nesaratai <nesaratai@gmail.com>
2025-05-29 16:24:41 -0400

    create the new state add two functions

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'react'
 
 export default [
   { ignores: ['dist'] },
@@ -22,12 +23,16 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off', // add this line
+      'react/no-unescaped-entities': 'off', // add this line
     },
   },
 ]
---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,7 @@
+
+const IngredientList = (props) => {
+    return <ul>// map through props.ingredients</ul>;
+  };
+  
+  export default IngredientList;
+  
